d6cdb1ebd62e608d8b0340dc47adf6a67f824ed9
ladypinetree <maureenpine@gmail.com>
2017-02-18 15:05:25 -0500

    Add in script to create poll

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,2 +1,5 @@
 class Poll < ApplicationRecord
+  # include Authentication
+  # has_many :votes
+  # validates :text, :user, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 20170217212455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_examples_on_user_id", using: :btree
+  end
+
+  create_table "polls", force: :cascade do |t|
+    t.text     "title"
+    t.text     "question"
+    t.text     "option1"
+    t.text     "option2"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
